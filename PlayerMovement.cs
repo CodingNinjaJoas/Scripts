d9e2aca85ab52879fcc682ab6dc2fc74ee904a8f
CodingNinjaJoas <50121774+CodingNinjaJoas@users.noreply.github.com>
2020-04-18 17:09:09 +0200

    re

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -9,21 +9,29 @@ public class PlayerMovement : MonoBehaviour
     public float jumpTime;
     public Animator anim;
     public float hitCoolDown;
+    public float shotHitCoolDown;
     private float cooldDown;
+    private float shotCoolDown;
     public GameObject hitFX;
+    public GameObject hitFXShot;
     public Transform transformFX;
     public Transform swordImpactPositionLeft;
     public Transform swordImpactPositionRight;
     public GameObject weaponColliderRight;
     public GameObject weaponColliderLeft;
+    public GameObject shotWeaponColliderLeft;
+    public GameObject shotWeaponColliderRight;
     public Transform feetPos;
     public float checkRadius;
     public LayerMask whatIsGround;
-    public float resetTime;
+    private float resetTime;
+    public float resetTimeShot;
+    public float resetTimeSword;
     public float health = 5;
     public float money = 0;
     public float food = 0;
     public float triggerImpactFXDelay = 0.2f;
+    public float triggerImpactFXDelayShot = 0.2f;
     private float moveInput;
     private Rigidbody2D rb;
     private float jumpTimeCounter;
@@ -45,7 +53,7 @@ public class PlayerMovement : MonoBehaviour
             {
                 weaponColliderRight.SetActive(true);
             }
-            if(moveDirection == true)
+            if (moveDirection == true)
             {
                 weaponColliderLeft.SetActive(true);
             }
@@ -55,9 +63,32 @@ public class PlayerMovement : MonoBehaviour
             boolToSetB = false;
             boolToSetS = "SwordHit";
             StartCoroutine("ResetVal");
+            resetTime = resetTimeSword;
         }
-       
     }
+       public void Shot()
+        {
+            if (shotCoolDown <= 0)
+            {
+                if (moveDirection == false)
+                {
+                    shotWeaponColliderRight.SetActive(true);
+                }
+                if (moveDirection == true)
+                {
+                    shotWeaponColliderLeft.SetActive(true);
+                }
+                shotCoolDown = shotHitCoolDown;
+                anim.SetBool("Shot", true);
+                StartCoroutine("TriggerFXShot");
+                boolToSetB = false;
+                boolToSetS = "Shot";
+                StartCoroutine("ResetVal");
+            resetTime = resetTimeShot;
+            }
+
+        }
+    
     public IEnumerator TriggerFX()
     {
         yield return new WaitForSeconds(triggerImpactFXDelay);
@@ -69,6 +100,19 @@ public class PlayerMovement : MonoBehaviour
         if (moveDirection == true)
         {
             g.transform.position = swordImpactPositionLeft.transform.position;
+        }
+    }
+    public IEnumerator TriggerFXShot()
+    {
+        yield return new WaitForSeconds(triggerImpactFXDelayShot);
+        GameObject g = Instantiate(hitFXShot, transformFX);
+        if (moveDirection == false)
+        {
+            g.transform.position = shotWeaponColliderRight.transform.position;
+        }
+        if (moveDirection == true)
+        {
+            g.transform.position = shotWeaponColliderRight.transform.position;
         }
     }
     public IEnumerator ResetVal()
@@ -84,7 +128,12 @@ public class PlayerMovement : MonoBehaviour
         {
             Sword();
         }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Shot();
+        }
         cooldDown -= Time.deltaTime;
+        shotCoolDown -= Time.deltaTime;
         if (health <= 0)
         {
             Die();
